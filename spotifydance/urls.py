ab7980804b05044c8a24ea2503c1208db995fd7f
mood <headlinecompare@hlcompare.com>
2021-05-27 22:46:47 -0400

    made start the homepage in urls.py

--- spotifydance/urls.py
+++ spotifydance/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.dancer, name='dancer'),
+    #path('', views.dancer, name='dancer'),
     path('viewer/', views.viewer, name='viewer'),
- path('start/', views.start, name='start'),
+ path('', views.start, name='start'),
  path('safari/', views.safari, name='safari'),
   path('land/', views.land, name='land'),
   path('empty_search/', views.empty_search, name='empty_search'),
